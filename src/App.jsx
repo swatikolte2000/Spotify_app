e512257e5472f95cbd1af9cfa33eb9e53732e709
swatikolte2000 <swatikolte2000@gmail.com>
2024-12-09 23:30:40 +0530

    half project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div  className='h-screen  bg-black'>
       <div className='h-[90%] flex'>
-
+       <Sidebar/>
       </div>
     
     </div>
