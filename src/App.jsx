cb57b2d591d82777a0372cef6fbd9801c7cc82a7
swatikolte2000 <swatikolte2000@gmail.com>
2024-12-09 21:59:03 +0530

    spotify app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div  className='h-screen  bg-black'>
+      <div className='h-[90%] flex'>
+
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
