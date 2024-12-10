263a93ce397f443fbfae698c24d8e4f502965286
swatikolte2000 <swatikolte2000@gmail.com>
2024-12-10 21:14:38 +0530

    spotify_clone project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useContext} from 'react'
+
 import './App.css'
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar.jsx'
+import Player from './components/Player.jsx'
+import Display from './components/Display.jsx'
+import { PlayerContext } from './context/PlayerContext.jsx'
 
 function App() {
 
+     const {audioRef,track} = useContext(PlayerContext);
 
   return (
     <div  className='h-screen  bg-black'>
       <div className='h-[90%] flex'>
        <Sidebar/>
+       <Display/>
       </div>
+      <Player/>
+      <audio ref={audioRef} src={track.file}  preload='auto'></audio>
     
     </div>
   )
